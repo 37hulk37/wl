1b88dd2509214576efea89fd5357c887f550a47d
Alexander Ushakov <alex.03spb@mail.ru>
2026-03-29 14:12:24 +0300

    added filter for vk & yandex

---
+++ filter.py
@@ -0,0 +1,40 @@
+import re
+
+keywords = {
+    "api.vk.com",
+    "ya.ru",
+}
+
+
+def filter(blocks):
+    filtered = []
+
+    for block in blocks:
+        match = re.search(r"sni=([^&]+)", block)
+
+        if not match:
+            continue
+
+        sni = match.group(1)
+        if not any(k in sni.lower() for k in keywords):
+            continue
+
+        filtered.append(block)
+
+    return filtered
+
+
+def __main__():
+    with open("vless_lite.txt", "r", encoding="utf-8") as f:
+        content = f.read()
+
+    blocks = re.split(r"(?=vless://)", content)
+
+    filtered = filter(blocks)
+
+    with open("vless_russians.txt", "w", encoding="utf-8") as f:
+        f.write("\n".join(filtered))
+
+
+if __name__ == "__main__":
+    __main__()
